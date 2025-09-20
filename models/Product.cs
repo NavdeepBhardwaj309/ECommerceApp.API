2936eae96ddb51cc55feaebd1dc6cbd4053263dd
navdeep bhardwaj <navdeep.bhardwaj309@gmail.com>
2025-09-21 00:12:18 +0530

    product model

--- models/Product.cs
+++ models/Product.cs
@@ -9,16 +9,16 @@ public class Product
     public int quantity { get; set; }
     public int totalInventory { get; set; }
 
-    // public Product(int _id, string _category, string _name, string _image, int _price, int _quantity, int _totalInventory)
-    // {
-    //     id = _id;
-    //     category = _category;
-    //     name = _name;
-    //     image = _image;
-    //     price = _price;
-    //     quantity = _quantity;
-    //     totalInventory = _totalInventory;
+    public Product(int _id, string _category, string _name, string _image, int _price, int _quantity, int _totalInventory)
+    {
+        id = _id;
+        category = _category;
+        name = _name;
+        image = _image;
+        price = _price;
+        quantity = _quantity;
+        totalInventory = _totalInventory;
 
         
-    // }
+    }
 }
